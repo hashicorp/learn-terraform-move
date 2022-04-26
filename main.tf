3c0e6505384c34061931d3e30762cf4b169292a3
Tu Nguyen <im2nguyen@gmail.com>
2022-04-26 11:41:25 -0700

    Add TFC compatibliity and default tags boilerplate

--- main.tf
+++ main.tf
@@ -1,19 +1,15 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = {
+      hashicorp-learn = "move-config"
     }
   }
-  required_version = ">= 1.1.0"
 }
-
 
 data "aws_availability_zones" "available" {
   state = "available"
-}
-
-provider "aws" {
-  region = var.region
 }
 
 module "vpc" {
